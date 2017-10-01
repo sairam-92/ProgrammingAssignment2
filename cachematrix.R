69a542ce5aad556c353f45cc4b38a66b468da073
sairam-92 <31654685+sairam-92@users.noreply.github.com>
2017-10-02 00:11:52 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+#The cachematrix.R file contains two functions, makeCacheMatrix() and cachesolve()
 
-## Write a short comment describing this function
+#makeCacheMatrix function is for caching the invers of the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+    inv <- NULL
+    
+    set <- function(y)
+    {
+        x <<- y
+        inv <<- NULL
+    }
+    
+    get <- function() x
+    
+    setinv <- function(inverse) 
+    {
+        inv <<- inverse
+    }
+    
+    getinv <- function() inv
+    
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
+## The cachesolve function checkes if the inverser of a matrix is null or already cached
 
-## Write a short comment describing this function
+#Based on the if function outcome returns the inverse from cache or computes 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#caches the inverse and returns the inverse
+
+cacheSolve <- function(x)
+{
+    inv <- x$getinv()
+    if(!is.null(inv)) 
+    {
+        message("getting cached data")
+        return(inv)
+    }
+    data <- x$get()
+    
+    #Error handling to check if the determinant of the given matrix input is 0
+    
+    MatDeterminant<-det(data)
+    if(MatDeterminant==0)
+    {
+        message("Determinant of matrix is 0,Matrix cannot be inverted, please choose differnt matrix data set")
+    }
+    else 
+    {
+        inv <- solve(data)
+        x$setinv(inv)
+        inv
+    }
 }
